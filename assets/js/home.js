0b25b7798391bb4a5ed43fa200225268b3dbc5d0
VooDoo <dearvoodoo.yt@gmail.com>
2023-11-23 06:08:43 +0100

    Add files via upload

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
     $.get("./assets/json/home.json", function(data) {
         $.each(data, function(i, item){
             console.log(item)
             $("#home_page").append(`
-                <col>
-                    <div class="card me-3" id="card-menu">
-                        <div class="card-body">
-                            <h5 class="card-title">${item['title']}</h5>
-                            <p class="card-text">${item['description']}</p>
-                            <a href="${item['button']['link']}" class="btn btn-${item['button']['type']}"><i class="${item['button']['icon']} me-2"></i>${item['button']['text']}</a>
+                    <div class="col">
+                        <div class="card me-3" id="card-menu">
+                            <div class="card-body">
+                                <h5 class="card-title">${data[i]['title']}</h5>
+                                <p class="card-text">${data[i]['description']}</p>
+                                <a href="${data[i]['button']['link']}" class="btn btn-${data[i]['button']['type']}"><i class="${data[i]['button']['icon']} me-2"></i>${data[i]['button']['text']}</a>
+                            </div>
                         </div>
                     </div>
-                </col>
-            `)
+                `
+            )
         })
     })
 })
